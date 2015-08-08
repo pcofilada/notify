7aabca406fdb4b0746a5e5ddc305b4a401a4a7b4
Patrick <pcofilada@gmail.com>
2015-08-08 15:58:48 +0800

    Add channel update action

--- app/controllers/api/v1/channels_controller.rb
+++ app/controllers/api/v1/channels_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ChannelsController < ApplicationController
   before_action :authenticate_with_token!, only: [:index, :create]
+  before_action :set_channel, only: :update
 
   def index
     channels = current_user.channels
@@ -17,7 +18,19 @@ class Api::V1::ChannelsController < ApplicationController
     end
   end
 
+  def update
+    if @channel.update(channel_params)
+      render json: @channel, status: 201
+    else
+      render json: { errors: @channel.errors.full_messages }, status: 422
+    end
+  end
+
 private
+
+  def set_channel
+    @channel = current_user.channels.find(params[:id])
+  end
 
   def channel_params
     params.require(:channel).permit(:logo, :status, :title, :description, :category)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: :create do
-        resources :channels, only: [:index, :create]
+        resources :channels, only: [:index, :create, :update]
       end
       resources :sessions, only: [:create, :destroy]
     end
