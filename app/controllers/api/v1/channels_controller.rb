fd996e287e88a94ae81de37d0e4c129ac1bdc1b2
Patrick <pcofilada@gmail.com>
2015-08-08 15:38:53 +0800

    Add channels controller create action

--- app/controllers/api/v1/channels_controller.rb
+++ app/controllers/api/v1/channels_controller.rb
@@ -1,10 +1,26 @@
 class Api::V1::ChannelsController < ApplicationController
-  before_action :authenticate_with_token!
+  before_action :authenticate_with_token!, only: [:index, :create]
 
   def index
     channels = current_user.channels
 
     render json: channels, status: 200 
+  end
+
+  def create
+    channel = current_user.channels.new(channel_params)
+
+    if channel.save
+      render json: channel, status: 201
+    else
+      render json: { errors: channel.errors }, status: 422 
+    end
+  end
+
+private
+
+  def channel_params
+    params.require(:channel).permit(:logo, :status, :title, :description, :category)
   end
 
 end
--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,3 +1,8 @@
 class Channel < ActiveRecord::Base
   belongs_to :owner, polymorphic: true
+
+  has_attached_file :logo, :styles => { :thumb => "50x50#" }
+  validates_attachment_content_type :logo, :content_type => /^image\/(png|jpg|jpeg)/
+
+  validates :logo, :status, :title, :description, :category, :owner, presence: true
 end
