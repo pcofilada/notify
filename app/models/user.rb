d20a622d604a7cc9dcfc443cd4057ca6e2d844ac
Patrick <pcofilada@gmail.com>
2015-08-08 13:32:14 +0800

    Add paperclip has_attached_file and validates_attachment content_type on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_attached_file :avatar, :styles => { :thumb => "50x50#" }
+  validates_attachment_content_type :avatar, :content_type => /^image\/(png|jpg|jpeg)/
+
   validates :first_name, :last_name, :age, :mobile_number, presence: true
   validates :auth_token, uniqueness: true
 end
