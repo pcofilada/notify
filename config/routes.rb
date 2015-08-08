5d27c0de7dd8aefef74936ac6ba0fa711fd21cb8
Patrick <pcofilada@gmail.com>
2015-08-08 14:15:31 +0800

    Add api sessions create and destroy endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: :create
+      resources :sessions, only: [:create, :destroy]
     end
   end
 end
